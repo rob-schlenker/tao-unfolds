e9d35bdffc5b6dda2e5de268e092efa7e1d61a33
rob-schlenker <schlenk7@gmail.com>
2025-03-07 10:53:19 -0500

    update footer to include creator's name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="p-4 text-center text-gray-500 text-sm">
-      <p>Made with the Tao • 2025</p>
+      <p>Made with the Tao • Created by Rob Schlenker • 2025</p>
     </footer>
   );
 };
